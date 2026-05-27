9af4252205ccff5b03530d3cd27a68405a2c4fc5
TazeemTayob17 <97630870+TazeemTayob17@users.noreply.github.com>
2026-05-27 14:52:07 +0200

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const count = new Map();
+
+        for(const num of nums){
+            count.set(num, (count.get(num) || 0) + 1);
+        }
+        
+        const sortedCounts = new Map(
+            [...count.entries()].sort((a, b) => b[1] - a[1])
+        );
+
+        const output = [...sortedCounts.keys()].slice(0,k);
+        return output;
+    }
+}
